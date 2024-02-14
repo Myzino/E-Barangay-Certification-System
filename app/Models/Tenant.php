7e1150ac6ffb60333111d1a05a96d2802e4fe4fa
jeffreysedoro <jeffreysedoro5@gmail.com>
2024-02-15 03:23:01 +0800

    Domain insertion and implementation

---
+++ app/Models/Tenant.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
+use Stancl\Tenancy\Contracts\TenantWithDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDomains;
+
+class Tenant extends BaseTenant implements TenantWithDatabase
+{
+    use HasDatabase, HasDomains;
+    
+    public static function getCustomColumns():array
+    {
+        return [
+            'id',
+            'name',
+            'email',
+            'password'
+        ];
+    }
+    public function setPasswordAttribute($value){
+        return $this->attributes['password'] = bcrypt($value);
+    }   
+}
