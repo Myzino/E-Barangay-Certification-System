8519c402ce9cc3edaff1529abacc318b377492d9
ramzel1414 <ramonpaulocaumban@gmail.com>
2024-02-26 19:00:12 +0800

    added admin profile page + update form + toastr

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Student;
 
 
 
@@ -34,4 +36,43 @@ class AdminController extends Controller
         
         return view('admin.admin_login');
     }
+
+    public function AdminProfile() {
+        
+        $id = Auth::user()->id;         //access user table authenticated field
+        $profileData = User::find($id);
+
+        return view('admin.admin_profile_view',compact('profileData')); //passing through compact method
+    }
+
+    public function AdminProfileStore(Request $request) {  //post request to update profile
+
+        $id = Auth::user()->id;         //access user table authenticated field
+        $data = User::find($id);
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+
+        if($request->file('photo')) {
+            $file = $request->file('photo');        //reqeusting the photo from database if naa
+            @unlink(public_path('upload/admin_images/'.$data->photo));  //replace previous admin image
+            $filename = date('YmdHi').$file->getClientOriginalName(); //to avoid similar photo conflict
+            $file->move(public_path('upload/admin_images'),$filename);
+            $data['photo'] = $filename;
+        }
+        $data->save();
+
+        //Toaster Part
+        $notification = array ( //toaster notif when updated
+            'message' => 'Admin Profile Updated Successfully',
+            'alert-type' => 'success',
+        );
+
+        return redirect()->back()->with($notification);
+    }
+
+
+
+
 }
--- resources/views/admin/body/header.blade.php
+++ resources/views/admin/body/header.blade.php
@@ -104,7 +104,7 @@
                             <i class="mdi mdi-account"></i>
                         </a>
                         <div class="dropdown-menu dropdown-menu-right">
-                            <a href="./app-profile.html" class="dropdown-item">
+                            <a href="{{route('admin.profile')}}" class="dropdown-item">
                                 <i class="icon-user"></i>
                                 <span class="ml-2">Profile </span>
                             </a>
